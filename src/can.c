2813bd9116b1beb4cb254ea122d4a358c039244e
Cubby DeBry <cubbydebry@gmail.com>
2025-11-10 18:01:07 -0700

    Working.... finally

--- src/can.c
+++ src/can.c
@@ -3,25 +3,46 @@
 #include <stdio.h>
 #include <pico/stdlib.h>
 #include <FreeRTOS.h>
+#include <queue.h>
 #include <task.h>
 
 static struct can2040 cbus;
+static QueueHandle_t queue;
 
 void send_task(void *params){
+    printf("CAN Send Task Started\n");
     struct can2040_msg msg = {0};
-    msg.id = 0x100;
-    msg.dlc = 1;
-    msg.data[0] = 42;
+    msg.id = 0;
+    msg.dlc = 8;
+    msg.data32[0] = 0x01234567;
+    msg.data32[1] = 0x89abcdef;
 
     while(1){
-        can2040_transmit(&cbus, &msg);
-        vTaskDelay(1000);
+        int ok = can2040_transmit(&cbus, &msg);
+        printf("TX enqueue: %d\n", ok);
+        // vTaskDelay(pdMS_TO_TICKS(1000));
+        busy_wait_ms(1000);
+    }
+}
+
+void receive_task(void *params){
+    printf("CAN Receive Task Started\n");
+    struct can2040_msg msg;
+    while(1){
+        // printf("Waiting for message...\n");
+        BaseType_t result = xQueueReceive(queue, &msg, pdMS_TO_TICKS(100));
+        // printf("xQueueReceive returned: %d\n", result);
+        if(result == pdTRUE){
+            printf("Message received: ID=0x%X DLC=%d DATA=0x%08X%08X\n", msg.id, msg.dlc, msg.data32[0], msg.data32[1]);
+        }
     }
 }
 
 static void can2040_cb(struct can2040 *cd, uint32_t notify, struct can2040_msg *msg)
 {
-    // Put your code here....
+    BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+    xQueueSendToBackFromISR(queue, msg, &xHigherPriorityTaskWoken);
+    portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
 }
 
 static void PIOx_IRQHandler(void)
@@ -31,6 +52,8 @@ static void PIOx_IRQHandler(void)
 
 void canbus_setup(void)
 {
+    queue = xQueueCreate(12, sizeof(struct can2040_msg));
+
     uint32_t pio_num = 0;
     uint32_t sys_clock = 125000000, bitrate = 500000;
     uint32_t gpio_rx = 4, gpio_tx = 5;
@@ -52,6 +75,9 @@ int main(void){
     stdio_init_all();
     canbus_setup();
     TaskHandle_t can_send_handle;
+    TaskHandle_t can_receive_handle;
     xTaskCreate(send_task, "CAN Send Task", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY+1, &can_send_handle);
+    xTaskCreate(receive_task, "CAN Receive Task", configMINIMAL_STACK_SIZE + 128, NULL, tskIDLE_PRIORITY+1, &can_receive_handle);
     vTaskStartScheduler();
+    return 0;
 }
